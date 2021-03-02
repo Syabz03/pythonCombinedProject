f1221092162dfa56f61ffd4eeb896ed15ab3ed38
Sum Yuan Loong <sumyuanloong@gmail.com>
2021-03-02 13:26:14 +0800

    addition of trawler method and notes

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import tweepy
 import praw
+import prawcore
 from mydata import *
 
 class crawler:
@@ -18,13 +19,20 @@ class redditCrawler(crawler):
 
     def __init__(self):
         self.reddit = praw.Reddit(
-            client_id='PESO3cS0KquaWQ', 
-            client_secret='ALSLenkZwZ5WCZ-32MaziUw-O7tmeA', 
-            user_agent='VanillaCast'
+            client_id='zo7beVRdF2cE8w', 
+            client_secret='NQtiZSmriDzkW6hY-HaACbG86Nytpw', 
+            user_agent='Java tests',
+            password = "no",
+            username="later"
         )
 
     def search(self,input):
-        input = "GPU"
+        #problem is that we are relying on reddit sorting system
+        # and unrelated post may come up
+        # i.e. GPU might come up in a escape from tarkov post
+
+        input = "hololive"
+        self.trawler(input)
         for submission in self.reddit.subreddit("all").search(input,'hot',limit=10):
             block = Mydata(input)
             block.addComment(submission.title)
@@ -33,9 +41,9 @@ class redditCrawler(crawler):
             iCount *= 100
             block.addLikeCount(int(iCount))
         
-        print(block.interactionCount)
-        for c in block.topComments:
-            print(c)
+        #print(block.interactionCount)
+        # for c in block.topComments:
+        #     print(c)
         
         return block
             # print("Title: ",submission.title)
@@ -47,6 +55,21 @@ class redditCrawler(crawler):
             # commentTree = submission.comments
             # print("comment 1",commentTree[0].body)
 
+    def trawler(self, input):
+        # check if there is a related subreddit
+        lst = self.reddit.subreddits.search(input,limit=1)
+        # c in this case is a subreddit obj
+        for c in lst:
+            print(c.display_name)
+            print(c.subscribers)
+            try:
+                var1=c.traffic() #var1 is a dict obj' IMPORTANT 
+                varlst = var1["day"] 
+                print(varlst[1])
+            except prawcore.NotFound:
+                print("not allowed to view trafic")
+                #always throws even when data is correct
+        
     
     def format(self, block):
         #return data obj
