ba658d851763b88665191c1ebc23c13117057ea1
Syabz03 <78629524+Syabz03@users.noreply.github.com>
2021-03-15 22:41:26 +0800

    removal of initial functions

--- JSONExport.py
+++ JSONExport.py
@@ -8,16 +8,13 @@ class retrieval:
     def __init__(self):
         pass
 
-    def testData(self,data):
-        pass
-
     def write_json(self, data, filename):
         pass
 
-    def reddit_save(self, searchQuery, rdt_results, queryLimit):
+    def exportData(self, data):
         pass
-
-    def twitter_save(self, searchQuery, tw_results):
+    
+    def combinedData(self, data):
         pass
 
 
@@ -51,8 +48,8 @@ class dataExport(retrieval):
         # check if file exists
         check_d_file = Path(data_file)
         check_p_file = Path(posts_file)
-        if not check_d_file.is_file(): self.write_json(data_temp, data_file)
-        if not check_p_file.is_file(): self.write_json(data_temp, posts_file)
+        # if not check_d_file.is_file(): self.write_json(data_temp, data_file)
+        # if not check_p_file.is_file(): self.write_json(data_temp, posts_file)
 
         with open(posts_file, 'r') as json_file:
             try:
@@ -80,72 +77,32 @@ class dataExport(retrieval):
 
             if source.lower() == to_dict['source'].lower():
                 data_temp.append(to_dict)
+        
+        self.combinedData(data, source, id)
+        #self.write_json(posts_temp, posts_file)
+        #self.write_json(data_temp, data_file)
 
-        self.write_json(posts_temp, posts_file)
-        self.write_json(data_temp, data_file)
+    def combinedData(self, data, source, id):
+        for submission in data:
+            to_dict = vars(submission)
+            d_date = dp.parse(str(to_dict['date']))
+            to_dict['date'] = str(d_date)
+            to_dict['top_comments'] = {}
 
-    # save reddit data to JSON file
-    def reddit_save(self, searchQuery, rdt_results, queryLimit):
-        temp = []
-        fields = ('id', 'author', 'title', 'url', 'score', 'selftext', 'num_comments', 'created_utc')
-        file_name = searchQuery + '_reddit.json'
-        reddit_file = Path(file_name)
-        if not reddit_file.is_file():
-            print("file does not exist, creating temp")  # created due to exception if file does not exist.
-            self.write_json(temp, file_name)
+            tc = submission.getTopComments()
+            for posts in tc:
+                posts_dict = vars(posts)
+                p_date = dp.parse(str(posts_dict['date']))
+                posts_dict['date'] = str(p_date)
+                top_comments = {}
 
-        with open(file_name, 'r') as json_file:
-            ids = []
-            try:
-                data = json.load(json_file)
-                temp = data
-                for id in temp: ids.append(id['id'])
-            except Exception as e:
-                print('Exception: ' + str(e))
+                if source.lower() in posts_dict['url'].lower() and posts_dict['id'] not in id:
+                    #print("found " + source)
+                    #posts_temp.append(posts_dict)
+                    top_comments.update(posts_dict)
+                
+                to_dict['top_comments'] = top_comments
+            print(to_dict['top_comments'])
 
-            s = set(ids)
-
-            for submission in rdt_results.hot(limit=5):
-                to_dict = vars(submission)
-                date = dp.parse(str(to_dict['created_utc']))
-                if to_dict['id'] not in s:
-                    sub_dict = {field: to_dict[field] for field in fields}
-                    sub_dict['author'] = str(submission.author)
-                    sub_dict['created_utc'] = str(date)
-                    temp.append(sub_dict)
-
-        self.write_json(temp, file_name)
-
-    # save twitter data to JSON file
-    def twitter_save(self, searchQuery, tw_results):
-        temp = []
-        file_name = searchQuery + '_twitter.json'
-        twitter_file = Path(file_name)
-        if not twitter_file.is_file():
-            print("file does not exist, creating temp")  # created due to exception if file does not exist.
-            self.write_json(temp, file_name)
-
-        # '''
-        with open(file_name, 'r') as json_file:
-            ids = []
-            try:
-                data = json.load(json_file)
-                temp = data
-                for id in temp: ids.append(id['id'])
-            except Exception as e:
-                print('Exception: ' + str(e))
-
-            s = set(ids)
-
-            for tweet in tw_results:
-                to_dict = vars(tweet)
-                date = dp.parse(str(to_dict['created_at']))
-                if to_dict['id'] not in s:
-                    sub_dict = {}
-                    sub_dict['id'] = tweet.id
-                    sub_dict['username'] = tweet.user.screen_name
-                    sub_dict['created_at'] = str(date)
-                    temp.append(sub_dict)
-
-        self.write_json(temp, file_name)
-        # '''
+            #if source.lower() == to_dict['source'].lower():
+                #data_temp.append(to_dict)
