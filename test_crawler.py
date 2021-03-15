a8a7482fa5a7f5fbb7464baac878fc122107a352
Sum Yuan Loong <sumyuanloong@gmail.com>
2021-03-15 19:22:02 +0800

    gitignore update init crawler test file

---
+++ test_crawler.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+class Testcrawler(TestCase):
+    pass
+
+class TestredditCrawler(TestCase):
+    def test_search(self):
+        self.fail()
+
+    def test_format(self):
+        self.fail()
+
+class TesttwitterCrawler(TestCase):
+    def test_search(self):
+        self.fail()
+
+    def test_format(self):
+        self.fail()
